c1966aea2ad438f6745bf4b810012996ddce4622
Rodrigo Vieira Batista dos Santos <rodrigo.v.santos89@gmail.com>
2022-03-07 09:06:10 -0300

    alterações sem efeito

--- modulo1/projeto-blackjack/desafio.js
+++ modulo1/projeto-blackjack/desafio.js
@@ -11,3 +11,57 @@
  * 
  */
 
+
+//IMPRIME O RESULTADO FINAL
+function imprimeResultadoFinal(user, PC) {
+   if (gameOver) {
+      //let the dealer take cards
+      while (
+         dealerScore < playerScore &&
+         playerScore <= 21 &&
+         dealerScore <= 21
+      ) {
+         dealerCards.push(getNextCard());
+         updateScores();
+      }
+   }
+   if (user > 21) {
+      playerWon = false;
+      gameOver = true;
+   } else if (PC > 21) {
+      playerWon = true;
+      gameOver = true;
+   } else if (gameOver) {
+      if (user > PC) {
+         playerWon = true;
+      } else {
+         playerWon = false;
+      }
+   }
+
+   //PEGA TODAS AS CARTAS DE UM JOGADOR E CONVERT EM STRING
+   function cartasParaString(jogador) { }
+
+   //GERA CARTAS INICIAIS PARA UM JOGADOR
+   function cartasIniciais(jogador) { }
+
+   //adiciona uma carta para um jogador
+   function adicionaUmaCarta(jogador) { }
+
+   alert("Boas vindas ao jogo de Blackjack!");
+
+   let jogandoBlackJack = true
+
+   while (jogandoBlackJack) {
+      if (confirm("Quer iniciar uma nova rodada?")) {
+
+      } else {
+         alert("O jogo acabou")
+         jogandoBlackJack = false
+
+      }
+
+   }
+
+
+
