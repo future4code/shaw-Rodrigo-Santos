789e1fea7120d6bec2c111ef36b5f5635327b38c
Rodrigo Vieira Batista dos Santos <rodrigo.v.santos89@gmail.com>
2022-03-25 15:48:14 -0300

    Comeco do projeto

---
+++ modulo2/projeto-whatslab/projeto-whatslab/src/App.js
@@ -0,0 +1,29 @@
+import logo from './logo.svg';
+import './App.css';
+import React from "react";
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      
+      </header>
+
+      <footer>
+        <input type="text" placeholder='Usuário' value=""/>
+
+
+
+
+      
+      </footer>
+      
+    </div>
+
+  );
+}
+
+export default App;
