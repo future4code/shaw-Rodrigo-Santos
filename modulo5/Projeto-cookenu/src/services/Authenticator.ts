6c06e69ceabeff2b809c80c6a298ae52554c34d8
Rodrigo Santos <rodrigo.v.santos89@gmail.com>
2022-06-30 13:01:21 -0300

    Fiz o Authenticator

--- modulo5/Projeto-cookenu/src/services/Authenticator.ts
+++ modulo5/Projeto-cookenu/src/services/Authenticator.ts
@@ -2,12 +2,22 @@ import * as jwt from "jsonwebtoken";
 import { USER_ROLES } from "../entities/User";
 
 export interface AuthenticationData {
-    id: string,
-    role: USER_ROLES
+  id: string;
+  role: USER_ROLES;
 }
 
 export class Authenticator {
   public generate(input: AuthenticationData): string {
+    const token = jwt.sign(input, process.env.JWT_KEY, {
+      expiresIn: process.env.ACESS_TOKEN_EXPIRES_IN,
+    });
+    return token;
+  }
 
+  // extrair as infos do token
+
+  public getTokenData(token: string): AuthenticationData {
+    const data =  jwt.verify(token, process.env.JWT_KEY)
+    return data as AuthenticationData;
   }
 }
