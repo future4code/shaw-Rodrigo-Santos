f2f382adde4f8e244c8b81abb7e6a4a9be829387
Rodrigo Santos <rodrigo.v.santos89@gmail.com>
2022-07-06 17:15:53 -0300

    Mexendo no migrations

--- modulo5/projeto-labook/src/migrations.ts
+++ modulo5/projeto-labook/src/migrations.ts
@@ -4,19 +4,20 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const connection: Knex = knex({
-     client: "mysql",
-     connection: {
-       host: process.env.DB_HOST,
-       port: 3306,
-       user: process.env.DB_USER,
-       password: process.env.DB_PASS,
-       database: process.env.DB_NAME,
-       multipleStatements: true
-     },
-   });
- 
+  client: "mysql",
+  connection: {
+    host: process.env.DB_HOST,
+    port: 3306,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    multipleStatements: true,
+  },
+});
+
 connection
-   .raw(`
+  .raw(
+    `
       CREATE TABLE IF NOT EXISTS labook_users(
          id VARCHAR(255) PRIMARY KEY,
          name VARCHAR(255) NOT NULL,
@@ -32,7 +33,27 @@ connection
          created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
          author_id VARCHAR(255),
          FOREIGN KEY (author_id) REFERENCES labook_users (id)
-      )
-   `)
-   .then(console.log)
-   .catch(console.log)
+      );
+
+      create table IF NOT EXISTS user_labook(
+         id varchar(255) primary key,
+         name varchar(255)
+      );
+         
+         create table IF NOT EXISTS postagem_labook(
+         id varchar(255) primary key,
+         postagem varchar(255) not null,
+         id_user varchar(255),
+         foreing key (id_user) references id(user)
+      );
+         
+         create table IF NOT EXISTS like_labook(
+         quantidadeLike int not null,
+         id_postagem varchar(255),
+         foreing key (id_postagem) references postagem(id)
+      );
+         
+   `
+  )
+  .then(console.log)
+  .catch(console.log);
